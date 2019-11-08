cc3e06ff1305fc66a1e1e18bca005770f110cfbd
Syeda Arisha <arshiasyed78@gmail.com>
2019-11-08 11:01:03 -0800

    Initial Commit

---
+++ Practice_HinderanceTests/Practice_HinderanceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Practice_HinderanceTests.swift
+//  Practice_HinderanceTests
+//
+//  Created by Syed Arisha on 08/11/2019.
+//  Copyright © 2019 Syed Arisha. All rights reserved.
+//
+
+import XCTest
+@testable import Practice_Hinderance
+
+class Practice_HinderanceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
